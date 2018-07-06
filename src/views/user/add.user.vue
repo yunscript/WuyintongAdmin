882f21f658b54ef2c1e7ab2cf5ebd95b4f40e496
yangjj <yangjinjin@qianbao.com>
2018-07-06 19:27:06 +0800

    多个两级全选功能的复用

--- src/views/user/add.user.vue
+++ src/views/user/add.user.vue
@@ -1,113 +1,111 @@
 <template>
-  <div class="app-container">
-    <p>用户基本信息</p>
-    <el-row :gutter="20">
-      <el-col :span="21">
-            <el-row :gutter="20" class="margin-bottom10">
-              <el-col :span="10" class="mb20">
-                <el-row>
-                  <el-col :span="8" class="text-right">
-                    用户名：
-                  </el-col>
-                  <el-col :span="16" class="text-left">
-                    <el-input placeholder=""></el-input>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="10" class="mb20">
-                <el-row>
-                  <el-col :span="8" class="text-right">
-                    初始密码：
-                  </el-col>
-                  <el-col :span="16" class="text-left">
-                    <el-input width='100' placeholder="请输入内容">
-                    </el-input>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="10" class="mb20">
-                <el-row>
-                  <el-col :span="8" class="text-right">
-                    姓名：
-                  </el-col>
-                  <el-col :span="16" class="text-left">
-                    <el-input placeholder=""></el-input>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="10" class="mb20">
-                <el-row>
-                  <el-col :span="8" class="text-right">
-                    职位：
-                  </el-col>
-                  <el-col :span="16" class="text-left">
-                    <el-input placeholder=""></el-input>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="10" class="mb20">
-                <el-row>
-                  <el-col :span="8" class="text-right">
-                    联系方式：
-                  </el-col>
-                  <el-col :span="16" class="text-left">
-                    <el-input placeholder=""></el-input>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="10" class="mb20">
-                <el-row>
-                  <el-col :span="8" class="text-right">
-                    角色：
-                  </el-col>
-                  <el-col :span="16" class="text-left">
-                    <el-select v-model="value" placeholder="请选择">
-                      <el-option
-                      v-for="item in options"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="10" class="mb20">
-                <el-row>
-                  <el-col :span="8" class="text-right">
-                    权限状态：
-                  </el-col>
-                  <el-col :span="16" class="text-left">
-                    <el-radio v-model="permissionStatus" label="1">禁用</el-radio>
-                    <el-radio v-model="permissionStatus" label="2">启用</el-radio>
-                  </el-col>
-                </el-row>
-              </el-col>
-            </el-row>
-      </el-col>
-    </el-row>
-    <p>用户权限设置</p>
-    <div class="checkbox margin-bottom10">
-      <el-checkbox v-model="homeChecked">首页</el-checkbox>
-    </div>
-    <div class="checkbox margin-bottom10">
-      <el-checkbox v-model="trade.selectAll" @change="handleAllChange(trade.selectAll, trade.menuLevel)">{{ trade.oneLable}}</el-checkbox>
-      <el-row class='ml15'>
-        <el-col :span="6" v-for="(items, index) in trade.menuLevel">
-          <div class="mt10">
-            <el-checkbox v-model="items.selectAll" @change="handleCheckAllChange(trade, items.selectAll, items.checkedCities, items.twoLable)">{{ items.oneLable }}</el-checkbox>
-            <div style="margin: 15px 0;"></div>
-            <el-checkbox-group v-model="items.checkedCities" @change="handleCheckedCitiesChange(trade, 'selectAll', index, items.twoLable, items.checkedCities)">
-              <el-checkbox class='ml15 block mb10' v-for="city in items.twoLable" :label="city" :key="city">{{city}}</el-checkbox>
-            </el-checkbox-group>
-          </div>
+<div class="app-container">
+  <p>用户基本信息</p>
+  <el-row :gutter="20">
+    <el-col :span="21">
+      <el-row :gutter="20" class="margin-bottom10">
+        <el-col :span="10" class="mb20">
+          <el-row>
+            <el-col :span="8" class="text-right">
+              用户名：
+            </el-col>
+            <el-col :span="16" class="text-left">
+              <el-input placeholder=""></el-input>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="10" class="mb20">
+          <el-row>
+            <el-col :span="8" class="text-right">
+              初始密码：
+            </el-col>
+            <el-col :span="16" class="text-left">
+              <el-input width='100' placeholder="请输入内容">
+              </el-input>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="10" class="mb20">
+          <el-row>
+            <el-col :span="8" class="text-right">
+              姓名：
+            </el-col>
+            <el-col :span="16" class="text-left">
+              <el-input placeholder=""></el-input>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="10" class="mb20">
+          <el-row>
+            <el-col :span="8" class="text-right">
+              职位：
+            </el-col>
+            <el-col :span="16" class="text-left">
+              <el-input placeholder=""></el-input>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="10" class="mb20">
+          <el-row>
+            <el-col :span="8" class="text-right">
+              联系方式：
+            </el-col>
+            <el-col :span="16" class="text-left">
+              <el-input placeholder=""></el-input>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="10" class="mb20">
+          <el-row>
+            <el-col :span="8" class="text-right">
+              角色：
+            </el-col>
+            <el-col :span="16" class="text-left">
+              <el-select v-model="value" placeholder="请选择">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+              </el-select>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="10" class="mb20">
+          <el-row>
+            <el-col :span="8" class="text-right">
+              权限状态：
+            </el-col>
+            <el-col :span="16" class="text-left">
+              <el-radio v-model="permissionStatus" label="1">禁用</el-radio>
+              <el-radio v-model="permissionStatus" label="2">启用</el-radio>
+            </el-col>
+          </el-row>
         </el-col>
       </el-row>
-    </div>
+    </el-col>
+  </el-row>
+  <p>用户权限设置</p>
+  <div class="checkbox margin-bottom10">
+    <el-checkbox v-model="homeChecked">首页</el-checkbox>
   </div>
+  <div class="checkbox margin-bottom10" v-for="(permissionItem, i) in permission">
+    <el-checkbox v-model="permissionItem.trade.selectAll" @change="handleAllChange(permissionItem.trade.selectAll, permissionItem.trade.menuLevel)">{{ permissionItem.trade.oneLable}}</el-checkbox>
+    <el-row class='ml15'>
+      <el-col :span="6" v-for="(items, index) in permissionItem.trade.menuLevel">
+        <div class="mt10">
+          <el-checkbox v-model="items.selectAll" @change="handleCheckAllChange(permissionItem.trade, items.selectAll, items.checkedCities, items.twoLable)">{{ items.oneLable }}</el-checkbox>
+          <div style="margin: 15px 0;"></div>
+          <el-checkbox-group v-model="items.checkedCities" @change="handleCheckedCitiesChange(permissionItem.trade, 'selectAll', index, items.twoLable, items.checkedCities)">
+            <el-checkbox class='ml15 block mb10' v-for="city in items.twoLable" :label="city" :key="city">{{city}}</el-checkbox>
+          </el-checkbox-group>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</div>
 </template>
 <script>
-import { fetchList } from '@/api/article'
+import {
+  fetchList
+} from '@/api/article'
 
 export default {
   data() {
@@ -146,33 +144,90 @@ export default {
       permissionStatus: '',
       homeChecked: false,
       financeChecked: false,
-      trade: {
-        oneLable: '融资管理', //  一级选项
-        // twoLable: ['融资申请', '授信管理', '用款查询', '还款查询'],
-        checkedCities: ['融资申请'],
-        selectAll: false,
-        menuLevel: [{
-          oneLable: '融资申请', //  一级选项
-          twoLable: ['正在审批', '审批记录', '授信批复', '查看企业信息', '修改授信批复', '授信审核', '查看审批记录'], //  二级选项
-          checkedCities: ['正在审批', '审批记录'], //  默认选中
-          selectAll: false  //  一级全选控制按钮
-        }, {
-          oneLable: '授信管理',
-          twoLable: ['查看授信详情'],
-          checkedCities: ['查看授信详情'],
-          selectAll: true  //  一级全选控制按钮
-        }, {
-          oneLable: '用款查询',
-          twoLable: ['查看用款详情', '放款确认', '查看放款凭证'],
-          checkedCities: ['查看用款详情', '查看放款凭证'],
-          selectAll: false
-        }, {
-          oneLable: '还款查询',
-          twoLable: ['账单明细', '还款详情'],
-          checkedCities: ['还款详情'],
-          selectAll: false  //  一级全选控制按钮
-        }]
-      }
+      permission: [{
+        trade: {
+          oneLable: '融资管理', //  一级选项
+          checkedCities: ['融资申请'],
+          selectAll: false,
+          menuLevel: [{
+            oneLable: '融资申请', //  一级选项
+            twoLable: ['正在审批', '审批记录', '授信批复', '查看企业信息', '修改授信批复', '授信审核', '查看审批记录'], //  二级选项
+            checkedCities: ['正在审批', '审批记录'], //  默认选中
+            selectAll: false //  一级全选控制按钮
+          }, {
+            oneLable: '授信管理',
+            twoLable: ['查看授信详情'],
+            checkedCities: ['查看授信详情'],
+            selectAll: true //  一级全选控制按钮
+          }, {
+            oneLable: '用款查询',
+            twoLable: ['查看用款详情', '放款确认', '查看放款凭证'],
+            checkedCities: ['查看用款详情', '查看放款凭证'],
+            selectAll: false
+          }, {
+            oneLable: '还款查询',
+            twoLable: ['账单明细', '还款详情'],
+            checkedCities: ['还款详情'],
+            selectAll: false //  一级全选控制按钮
+          }]
+        }
+      }, {
+        trade: {
+          oneLable: '链上数据查询', //  一级选项
+          checkedCities: [],
+          selectAll: false,
+          menuLevel: [{
+            oneLable: '企业基本信息查询', //  一级选项
+            twoLable: [], //  二级选项
+            checkedCities: [], //  默认选中
+            selectAll: false //  一级全选控制按钮
+          }, {
+            oneLable: '企业融资统计',
+            twoLable: [],
+            checkedCities: [],
+            selectAll: false //  一级全选控制按钮
+          }]
+        }
+      }, {
+        trade: {
+          oneLable: '消息管理', //  一级选项
+          checkedCities: ['融资申请'],
+          selectAll: false,
+          menuLevel: [{
+            oneLable: '我的消息', //  一级选项
+            twoLable: ['查看'], //  二级选项
+            checkedCities: [], //  默认选中
+            selectAll: false //  一级全选控制按钮
+          }, {
+            oneLable: '发送消息',
+            twoLable: ['查看', '修改', '删除', '撤回'],
+            checkedCities: ['查看'],
+            selectAll: false //  一级全选控制按钮
+          }]
+        }
+      }, {
+        trade: {
+          oneLable: '系统管理', //  一级选项
+          checkedCities: ['融资申请'],
+          selectAll: false,
+          menuLevel: [{
+            oneLable: '用户管理', //  一级选项
+            twoLable: ['修改', '删除', '新增用户', '角色增加', '角色删除'], //  二级选项
+            checkedCities: [], //  默认选中
+            selectAll: false //  一级全选控制按钮
+          }, {
+            oneLable: '系统消息',
+            twoLable: ['查看', '新增'],
+            checkedCities: ['查看'],
+            selectAll: false //  一级全选控制按钮
+          }, {
+            oneLable: '操作日志',
+            twoLable: ['查看'],
+            checkedCities: ['查看'],
+            selectAll: true //  一级全选控制按钮
+          }]
+        }
+      }]
     }
   },
   created() {
@@ -182,6 +237,7 @@ export default {
     handleAllChange(selectAll, options) {
       if (selectAll) {
         options.forEach((item, i) => {
+          console.log('options[i].checkedCities = ' + options[i].checkedCities)
           this.$set(options[i], 'selectAll', true)
           options[i].checkedCities.splice(0, options[i].checkedCities.length)
           options[i].twoLable.forEach((items, j) => {
@@ -235,7 +291,9 @@ export default {
       if (this.multipleSelection.length) {
         this.downloadLoading = true
         require.ensure([], () => {
-          const { export_json_to_excel } = require('vendor/Export2Excel')
+          const {
+            export_json_to_excel
+          } = require('vendor/Export2Excel')
           const tHeader = ['序号', '文章标题', '作者', '阅读数', '发布时间']
           const filterVal = ['id', 'title', 'author', 'pageviews', 'display_time']
           const list = this.multipleSelection
@@ -261,26 +319,33 @@ export default {
 .el-input {
   width: 200px;
 }
+
 .el-select {
   width: 120px;
 }
+
 p {
   color: white;
   font-size: 14px;
 }
+
 .checkbox {
   background-color: #272D4A;
   padding: 20px;
 }
+
 .mb20 {
   margin-bottom: 20px;
 }
+
 .mb10 {
   margin-bottom: 10px;
 }
+
 .mt10 {
   margin-top: 10px;
 }
+
 .ml15 {
   margin-left: 15px;
 }
