1987e28ef42ef80daeca9957c9ed1afc8d2fe40c
zhangqiuwen <1275843103@qq.com>
2018-08-08 15:44:46 +0800

    update request url

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,6 +2,6 @@ module.exports = {
     NODE_ENV: '"development"',
     ENV_CONFIG: '"dev"',
     // BASE_API: '"https://api-dev"',
-    BASE_API: '" http://localhost:8004/supplyfinance"',
+    BASE_API: '" http://192.168.18.35:8004/supplyfinance"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 }
---
+++ src/views/financing/repayment.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'repayment'
+  }
+</script>
+
+<style scoped>
+
+</style>
